01472ce823bc630881abe5b914395afc98dd16dd
libra_310 <jas31085@gmail.com>
2018-04-04 18:22:59 +0200

    Handbrake Converter

---
+++ Convert-first.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+############################################################
+#                   MKV script Converter
+#
+#   Convert MKV file whith 720p preset conversion
+#
+# 	Convert /full/path/to/file.mkv
+# 
+#   10/2017 Edoardo Marchetti
+#
+############################################################
+
+
+
+# full film Path
+RemoteMKV="$1"
+
+HANDBRAKE="$(which HandBrakeCLI)"
+
+# Nome del file
+NAME="$(echo "$RemoteMKV" | awk -F"/" '{ print $NF}')"
+
+# conversion tmp folder
+LocalMKV="/tmp/$NAME"
+
+# FolderOrigin
+FolderOrigin="$(echo $RemoteMKV | awk -v name="$NAME" '{ gsub(name, "") ; print $0 }')"
+
+# Local filename
+TempM4V="/video/$NAME.m4v"
+
+# Preset HandBrake
+#PRESET="Apple TV 2"
+PRESET="-e x264 -q 20.0 -r 30 --pfr  -a 1,1 -E ffaac,copy:ac3 -B 160,160 -6 dpl2,none -R Auto,Auto -D 0.0,0.0 --audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 -f mp4 -4 -X 1280 -Y 720 --decomb=fast --loose-anamorphic --modulus 2 -m -O --x264-preset medium --h264-profile high --h264-level 4.0"
+
+echo "Start Processing File  " $(date) 
+echo ""
+
+# moving the file locally (only if the source is on the network and with SSD)
+cp "$RemoteMKV" "/video/$NAME"
+
+# conversion task 
+$HANDBRAKE -v -i "$LocalMKV" $PRESET -o "$TempM4V"
+
+# moving the file to the original position
+cp "$TempM4V" "$FolderOrigin"
+
+# delete tmp file
+if [[ -e "$TempM4V" ]]; then
+ 	rm -rf "$TempM4V" 
+fi
+
+# delete mkv file
+# if [[ -e "$RemoteMKV" ]]; then
+# 	rm -rf "$RemoteMKV"
+# fi
+
+# delete local mkv
+if [[ -e "$LocalMKV" ]]; then
+	rm -rf "$LocalMKV"
+fi
+
+echo "Stop Processing File  " $(date)
+echo ""
---
+++ Convert.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+############################################################
+#                   MKV script Converter
+#
+#   Convert MKV file whith Handbracke-cli
+#
+# 
+# Usage: Convert -f /full/path/to/file.mkv [-l /local/folder/] [-p "preset"]
+#     -f, --file 				mkv File
+#     -l, --local 				local folder if you use SSD disk [default=/tmp/]
+# 	  -p, --preset 				Preset for conversion
+#     -h, --help 				Display this screen
+#
+# 
+# (c) 2018, Edoardo Marchetti <marchetti.edoardo@gmail.com>
+# https://github.com/jas31085/Libra_310-Script-Collection
+#
+
+
+
+if [[ $# -eq 0 ]] ; then
+	sed -n '2,17p' "$0" | tr -d '#'
+	exit 3
+fi
+
+if [[ -e "$(which HandBrakeCLI)" ]]; then
+	HANDBRAKE="$(which HandBrakeCLI)"
+else
+	echo "ERROR: Install HandBrakeCLI"
+	exit 3
+fi
+
+while [[ -n "$1" ]]; do
+  case $1 in
+    --file | -f)
+		if [[ -e $2 ]]; then
+			# full film Path
+			RemoteMKV="$2"
+			NAME="$(echo "$RemoteMKV" | awk -F"/" '{ print $NF}')"
+			FolderOrigin="$(echo $RemoteMKV | awk -v name="$NAME" '{ gsub(name, "") ; print $0 }')"
+		else
+			echo "ERROR: the file doesn't exist"
+			exit 3
+		fi
+      shift
+      ;;
+    --local | -l)
+		if [[ -d "$2" ]]; then
+			LocalMKV="$2$NAME"
+		else
+			echo "ERROR: the local temp doesn't exist"
+			exit 3
+		fi
+      shift
+      ;;
+    --preset | -p)
+		PRESET="$2"
+      shift
+      ;;
+    --help | -h | *)
+      sed -n '2,17p' "$0" | tr -d '#'
+      exit 3
+      ;;
+  esac
+  shift
+done
+
+# conversion tmp folder
+if [[ -z "$LocalMKV" ]]; then
+	LocalMKV="/tmp/$NAME"
+fi
+
+# Local filename
+TempM4V="$LocalMKV.m4v"
+
+
+# Preset HandBrake
+if [[ -z "$PRESET" ]]; then
+	PRESET="-e x264 -q 20.0 -r 30 --pfr  -a 1,1 -E ffaac,copy:ac3 -B 160,160 -6 dpl2,none -R Auto,Auto -D 0.0,0.0 --audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 -f mp4 -4 -X 1280 -Y 720 --decomb=fast --loose-anamorphic --modulus 2 -m -O --x264-preset medium --h264-profile high --h264-level 4.0"
+fi
+
+echo "Start Processing File  " $(date) 
+echo ""
+
+# moving the file locally (only if the source is on the network and with SSD)
+cp "$RemoteMKV" "$LocalMKV"
+
+# conversion task 
+$HANDBRAKE -v -i "$LocalMKV" $PRESET -o "$TempM4V"
+
+# moving the file to the original position
+cp "$TempM4V" "$FolderOrigin"
+
+# delete tmp file
+if [[ -e "$TempM4V" ]]; then
+ 	rm -rf "$TempM4V" 
+fi
+
+# delete mkv file
+# if [[ -e "$RemoteMKV" ]]; then
+# 	rm -rf "$RemoteMKV"
+# fi
+
+# delete local mkv
+if [[ -e "$LocalMKV" ]]; then
+	rm -rf "$LocalMKV"
+fi
+
+echo "Stop Processing File  " $(date)
+echo ""
